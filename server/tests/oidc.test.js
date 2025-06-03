86e8f24ad9b3b7fee6f0d63a27b8647ddeae441c
aartoni <alessio@artoni.org>
2025-06-03 15:23:52 +0200

    feat: add playground for tests

---
+++ server/tests/oidc.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const { setupStrapi, stopStrapi } = require('../../playground/tests/helpers');
+
+let strapi;
+
+beforeAll(async () => {
+  strapi = await setupStrapi();
+});
+
+afterAll(async () => {
+  await stopStrapi();
+});
+
+describe('GET /oidc', () => {
+  it('should redirect with 302 and set session values', async () => {
+    const res = await request(strapi.server.httpServer).get('/api/strapi-plugin-sso/oidc');
+
+    expect(res.status).toBe(302);
+    expect(res.headers['location']).toMatch(/^https:\/\/auth.example.com\/authorize/);
+    const url = new URL(res.headers['location']);
+    expect(url.searchParams.get('response_type')).toBe('code');
+    expect(url.searchParams.get('client_id')).toBeDefined();
+    expect(url.searchParams.get('code_challenge')).toBeDefined();
+    expect(url.searchParams.get('state')).toBeDefined();
+  });
+});
