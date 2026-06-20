c82fde6eb6b36c80a3cac0ad806647996b146bd5
aartoni <alessio@artoni.org>
2026-06-20 17:37:42 +0200

    ref: format ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,24 +7,28 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("eslint:recommended"), {
+export default [
+  ...compat.extends("eslint:recommended"),
+  {
     ignores: ["**/data/", "**/dist/", "**/.yalc/", "**/.strapi/"],
-}, {
+  },
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.commonjs,
-            strapi: "readonly",
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+        strapi: "readonly",
+      },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
 
     rules: {},
-}];
+  },
+];
