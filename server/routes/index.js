c90f604810ff098595835d4c4c38e34019ae8e39
aartoni <alessio@artoni.org>
2026-06-20 21:19:53 +0200

    fix: new routes structure

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,91 +1,48 @@
-export default [
-  {
-    method: 'GET',
-    path: '/google',
-    handler: 'google.googleSignIn',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/google/callback',
-    handler: 'google.googleSignInCallback',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/cognito',
-    handler: 'cognito.cognitoSignIn',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/cognito/callback',
-    handler: 'cognito.cognitoSignInCallback',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/azuread',
-    handler: 'azuread.azureAdSignIn',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/azuread/callback',
-    handler: 'azuread.azureAdSignInCallback',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/sso-roles',
-    handler: 'role.find'
-  },
-  {
-    method: 'PUT',
-    path: '/sso-roles',
-    handler: 'role.update'
-  },
-  {
-    method: 'GET',
-    path: '/oidc',
-    handler: 'oidc.oidcSignIn',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/oidc/callback',
-    handler: 'oidc.oidcSignInCallback',
-    config: {
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/whitelist',
-    handler: 'whitelist.info',
-  },
-  {
-    method: 'POST',
-    path: '/whitelist',
-    handler: 'whitelist.register'
-  },
-  {
-    method: 'DELETE',
-    path: '/whitelist/:id',
-    handler: 'whitelist.removeEmail'
+export default {
+  'content-api': {
+    type: 'content-api',
+    routes: [
+      {
+        method: 'GET',
+        path: '/sso-roles',
+        handler: 'role.find'
+      },
+      {
+        method: 'PUT',
+        path: '/sso-roles',
+        handler: 'role.update'
+      },
+      {
+        method: 'GET',
+        path: '/oidc',
+        handler: 'oidc.oidcSignIn',
+        config: {
+          auth: false,
+        },
+      },
+      {
+        method: 'GET',
+        path: '/oidc/callback',
+        handler: 'oidc.oidcSignInCallback',
+        config: {
+          auth: false,
+        },
+      },
+      {
+        method: 'GET',
+        path: '/whitelist',
+        handler: 'whitelist.info',
+      },
+      {
+        method: 'POST',
+        path: '/whitelist',
+        handler: 'whitelist.register'
+      },
+      {
+        method: 'DELETE',
+        path: '/whitelist/:id',
+        handler: 'whitelist.removeEmail'
+      }
+    ]
   }
-];
+};
