bebb59a5fff652e216ed0cdc490a802265dba3e2
aartoni <alessio@artoni.org>
2026-06-21 11:15:09 +0200

    feat: Cypress DNS resolution

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'cypress';
 import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
 
 export default defineConfig({
+  hosts: {
+    'cms.strapi.local': '127.0.0.1',
+    'auth.strapi.local': '127.0.0.1',
+  },
   e2e: {
-    baseUrl: 'http://localhost:1337',
+    baseUrl: 'https://cms.strapi.local',
     specPattern: '**/*.cy.{js,ts,jsx,tsx}',
     video: true,
     defaultCommandTimeout: 10000,
