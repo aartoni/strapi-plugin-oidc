9e062417a3d6cd651e7d060c91f4d70066d6f65d
aartoni <alessio@artoni.org>
2026-06-25 17:47:05 +0200

    ref: simplify webhook trigger

--- server/services/oauth.js
+++ server/services/oauth.js
@@ -41,30 +41,24 @@ export default ({ strapi }) => ({
     return ctx.acceptsLanguages("en", "fr") || "en";
   },
   async triggerWebHook(user) {
-    let ENTRY_CREATE;
-    const webhookStore = strapi.serviceMap.get("webhookStore");
-    const eventHub = strapi.serviceMap.get("eventHub");
+    const eventHub = strapi.eventHub;
+    if (!eventHub) return;
 
-    if (webhookStore) {
-      ENTRY_CREATE = webhookStore.allowedEvents.get("ENTRY_CREATE");
-    }
     const modelDef = strapi.getModel("admin::user");
     const sanitizedEntity =
-      await strapiUtils.sanitize.sanitizers.defaultSanitizeOutput(
-        {
-          schema: modelDef,
-          getModel: (uid2) => strapi.getModel(uid2),
-        },
+      await await strapiUtils.sanitize.sanitizers.defaultSanitizeOutput(
+        { schema: modelDef, getModel: (uid) => strapi.getModel(uid) },
         user,
       );
-    eventHub.emit(ENTRY_CREATE, {
+
+    eventHub.emit("entry.create", {
       model: modelDef.modelName,
       entry: sanitizedEntity,
     });
   },
   triggerSignInSuccess(user) {
     const { password, ...safeUser } = user;
-    const eventHub = strapi.serviceMap.get("eventHub");
+    const eventHub = strapi.eventHub;
     eventHub.emit("admin.auth.success", {
       user: safeUser,
       provider: "strapi-plugin-sso",
