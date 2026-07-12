3b3f3df4a018dbeda773f06541ef2f00137a64c3
aartoni <alessio@artoni.org>
2026-07-12 21:33:24 +0200

    feat: update OIDC scopes

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -7,7 +7,7 @@ export default {
     OIDC_REDIRECT_URI: "http://localhost:1337/api/oidc/callback",
     OIDC_CLIENT_ID: "",
     OIDC_CLIENT_SECRET: "",
-    OIDC_SCOPES: "openid profile email",
+    OIDC_SCOPES: "openid profile email groups",
     OIDC_AUTHORIZATION_ENDPOINT: "",
     OIDC_TOKEN_ENDPOINT: "",
     OIDC_USER_INFO_ENDPOINT: "",
