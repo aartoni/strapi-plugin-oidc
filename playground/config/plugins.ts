bbfc4b8ed30d30905d00cd988a8ce2c592aee5fe
aartoni <alessio@artoni.org>
2026-07-12 21:29:25 +0200

    ref: read playground client ID + secret from fs

--- playground/config/plugins.ts
+++ playground/config/plugins.ts
@@ -1,13 +1,19 @@
+import { readFileSync } from "node:fs";
 import { Core } from "@strapi/strapi";
 
+const fromFile = (filePath: string | undefined) => {
+  if (!filePath) return undefined;
+  return readFileSync(filePath, "utf8").trim();
+};
+
 export default ({ env }: Core.Config.Shared.ConfigParams) => ({
-  "oidc": {
+  oidc: {
     enabled: true,
     config: {
       REMEMBER_ME: false,
       OIDC_REDIRECT_URI: env("OIDC_REDIRECT_URI"),
-      OIDC_CLIENT_ID: env("OIDC_CLIENT_ID"),
-      OIDC_CLIENT_SECRET: env("OIDC_CLIENT_SECRET"),
+      OIDC_CLIENT_ID: fromFile(env("OIDC_CLIENT_ID_FILE")),
+      OIDC_CLIENT_SECRET: fromFile(env("OIDC_CLIENT_SECRET_FILE")),
       OIDC_AUTHORIZATION_ENDPOINT: env("OIDC_AUTHORIZATION_ENDPOINT"),
       OIDC_TOKEN_ENDPOINT: env("OIDC_TOKEN_ENDPOINT"),
       OIDC_USER_INFO_ENDPOINT: env("OIDC_USER_INFO_ENDPOINT"),
